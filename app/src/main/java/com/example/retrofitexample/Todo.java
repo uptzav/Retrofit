a2dff9d37482d0414e8fc47411bc8ae547da5687
uptzav <37610108+uptzav@users.noreply.github.com>
2020-11-30 19:00:24 -0500

    Retrofit
    Retrofit

---
+++ app/src/main/java/com/example/retrofitexample/Todo.java
@@ -0,0 +1,32 @@
+package com.example.retrofitexample;
+
+public class Todo {
+   private int userID;
+    private int    Id;
+    private  String title;
+    private  Boolean completed;
+
+    public int getUserID() {
+        return userID;
+    }
+
+
+
+    public int getId() {
+        return Id;
+    }
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+
+}
